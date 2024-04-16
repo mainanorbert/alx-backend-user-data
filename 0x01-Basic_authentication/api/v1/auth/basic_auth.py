05a4f88ee6489610e69f0c2625265426f3d993ab
Norberts <mainanorbert@gmail.comm>
2024-04-16 11:08:22 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -63,6 +63,8 @@ class BasicAuth(Auth):
         users = User.search({'email': user_email})
         if not users:
             return None
+        if len(users) == 0:
+            return None
         user = users[0]
         if not user.is_valid_password(user_pwd):
             return None
