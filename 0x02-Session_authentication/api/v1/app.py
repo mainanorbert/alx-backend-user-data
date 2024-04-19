d775d6c698cb71103f578c3297ea5b0311ae377f
Norberts <mainanorbert@gmail.comm>
2024-04-19 08:58:46 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,9 +32,13 @@ def before_request():
     """function that runs before each request"""
     if auth is None:
         return
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    paths = ['/api/v1/status/', '/api/v1/auth_session/login/',
+             '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if not auth.require_auth(request.path, paths):
         return
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
+        abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
