4603e82ae31adfccfce8237e2a43446c52d666af
Norberts <mainanorbert@gmail.comm>
2024-04-23 21:33:58 +0300

    logging out user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,11 +46,10 @@ def logging_out() -> str:
     """deleting user"""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user not None:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
+    if user is None:
         abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
