88e9a5873b91af21b6e7df80d984b38800af81ed
Norberts <mainanorbert@gmail.comm>
2024-04-23 20:53:08 +0300

    find user by session if

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,3 +55,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(session_id: str) -> Optional[User, None]:
+        """finding user by session id"""
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
