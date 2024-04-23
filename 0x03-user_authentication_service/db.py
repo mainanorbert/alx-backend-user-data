c8a900549a8555de1e3272852ee562792090855a
Norberts <mainanorbert@gmail.comm>
2024-04-23 14:44:41 +0300

    add, update, find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
+'''module for db class for storing user'''
 
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
-    """DB class
+    """Class for db class
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initializing a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,20 +31,14 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-        """
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-            return user
-        except IntegrityError:
-            # Handle integrity error (e.g., duplicate email)
-            self._session.rollback()
-            return None
+        """Adding user to the db"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """method to find user using kwargs"""
+        """Finding user in db with kwargs"""
         try:
             return self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
@@ -55,11 +47,12 @@ class DB:
             raise InvalidRequestError()
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """. update user"""
+        """Update the attributes of a user.
+        """
         user = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
-            if hasattr(User, k):
-                setattr(User, k, v)
+            if hasattr(user, k):
+                setattr(user, k, v)
             else:
                 raise ValueError()
         self._session.commit()
