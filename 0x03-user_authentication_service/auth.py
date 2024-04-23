a713dd0ffd6334cef6531a54f4e03271548b9ccc
Norberts <mainanorbert@gmail.comm>
2024-04-23 21:17:52 +0300

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,6 +69,6 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """method that destroys a session"""
-        id user_id is None:
+        if user_id is None:
             return None
         self._db.update_user(user_id, session_id=None)
