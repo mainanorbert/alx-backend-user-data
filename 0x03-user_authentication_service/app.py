331ad9dd3c5f048642dbc1ae0b6aa187cd044192
Norberts <mainanorbert@gmail.comm>
2024-04-23 20:46:16 +0300

    adding login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """flask app"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -25,6 +25,20 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """login route"""
+    email: str = request.form.get('email')
+    password: str = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": f"{email}", "message": "logged in"})
+        res.set_cookie("session_id", session_id)
+        return res, 200
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
