a91ff54120e6aece8d88c303cf2208145c1ba3b1
Norberts <mainanorbert@gmail.comm>
2024-04-24 18:22:00 +0300

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
         self._db.update_user(user.id, reset_token=token)
         return token
 
-    def update_password(self, reset_token: str, new_password: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         """updating password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
@@ -92,3 +92,4 @@ class Auth:
         hashed_password = _hashed_password(new_password)
         self._db.update_user(user.id,
                              reset_token=None, hashed_password=hashed_password)
+        return None
